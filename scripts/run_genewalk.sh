7c05eda696f62647714a09220b4b8b10d5f99fe6
Clemens Hug <clemens.hug@gmail.com>
2020-01-15 10:05:52 -0500

    Add shell script for genewalk

---
+++ scripts/run_genewalk.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH -p short
+#SBATCH -J genewalk
+#SBATCH -t 0-12:00
+#SBATCH -c 4
+#SBATCH --mem=8000
+
+genes="$1"
+filename=$(basename "$genes")
+project="${filename%.*}"
+
+set -eux
+
+genewalk --project "$project" --base_folder $(pwd) --id_type ensembl_id
