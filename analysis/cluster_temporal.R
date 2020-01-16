e2bdae9d126cd2e7bfd2ff5104e8219a545d257f
Clemens Hug <clemens.hug@gmail.com>
2020-01-15 21:58:34 -0500

    Also calculate variance between ERKi doses

--- analysis/cluster_temporal.R
+++ analysis/cluster_temporal.R
@@ -93,41 +93,6 @@ plot_clusters_line_by_erki <- function(d, cluster) {
     facet_grid(vars(ERKi), vars(!!cluster_quo))
 }
 
-plot_cluster_trajectories <- function(
-  data, x, y, order_id, trajectory_id, facet_x = NULL, facet_y = NULL,
-  all_traces = FALSE
-) {
-  x_quo <- enquo(x)
-  y_quo <- enquo(y)
-  order_id_quo <- enquo(order_id)
-  trajectory_id_quo <- enquo(trajectory_id)
-  facet_x_quo <- enquo(facet_x)
-  facet_y_quo <- enquo(facet_y)
-  averages <- data %>%
-    group_by(!!x_quo, !!facet_x_quo, !!facet_y_quo) %>%
-    summarize_at(vars(!!y_quo), mean) %>%
-    ungroup() %>%
-    arrange(!!order_id_quo)
-  p <- ggplot(averages, aes(!!x_quo, !!y_quo, color = !!order_id_quo))
-  if (all_traces)
-    p <- p +
-    geom_path(
-      aes(group = !!trajectory_id_quo),
-      data = data %>% arrange(!!order_id_quo), size = .8, alpha = 0.1, color = "#CCCCCC"
-    )
-  p <- p +
-    geom_path() +
-    geom_point() +
-    theme_minimal() +
-    scale_color_viridis_c() +
-    theme(legend.position = "bottom")
-  if (!missing(facet_x) || !missing(facet_y))
-    p <- p +
-    facet_grid(vars(!!facet_y_quo), vars(!!facet_x_quo), scales = "free")
-  p
-}
-
-
 # Temporal clustering k-medoids and hierarchical -------------------------------
 ###############################################################################T
 
@@ -250,35 +215,63 @@ temporal_ordering_avg_col_stats <- lfc_long %>%
   group_by(gene_id, gene_name, ERKi) %>%
   mutate(
     log2FoldChange_rescaled = log2FoldChange %>%
-      {if (.[order(abs(.), decreasing = TRUE)[1]] > 0) scales::rescale(.) else (scales::rescale(-.))},
-    log2FoldChange_var = var(log2FoldChange)
+      abs() %>%
+      scales::rescale(),
+    log2FoldChange_var_time = var(log2FoldChange)
   ) %>%
   ungroup() %>%
   group_by(gene_id, gene_name, Time) %>%
-  mutate(log2FoldChange_rescaled_mean = weighted.mean(log2FoldChange_rescaled, log2FoldChange_var)) %>%
+  # group_modify(
+  #   function(df, g) {
+  #     rescaled_cor <- df %>%
+  #       select()
+  #   }
+  # )
+  mutate(
+    log2FoldChange_rescaled_mean = weighted.mean(log2FoldChange_rescaled, log2FoldChange_var_time),
+    log2FoldChange_rescaled_var_erki = Hmisc::wtd.var(log2FoldChange_rescaled, log2FoldChange_var_time, method = "ML")
+  ) %>%
   ungroup()
+
+# temporal_ordering_avg_col_stats_hetero <- temporal_ordering_avg_col_stats %>%
+#   group_by(gene_id, gene_name, Time)
+#
+# lfc_long %>%
+#   inner_join(condition_meta, by = "condition") %>%
+#   filter(gene_name == "DUSP1", DOX == 1) %>%
+#   select(gene_id, Time, ERKi, log2FoldChange) %>%
+#   spread(ERKi, log2FoldChange) %>%
+#   GGally::ggpairs(columns = 3:ncol(.))
+#
+
 
 temporal_ordering_avg_col <- temporal_ordering_avg_col_stats %>%
   group_by(gene_id, gene_name) %>%
+  # group_modify(
+  #   function(df, g) {
+  #
+  #   }
+  # )
   summarize(
     max_induction = Time[order(log2FoldChange_rescaled_mean, decreasing = TRUE)[1]],
     # In extremely rare cases log2FoldChange_rescaled_mean never reaches 0.5 in
     # any condition, just taking the time point with max induction then
-    mid_induction = if (!any(log2FoldChange_rescaled_mean > 0.5)) max_induction else Time[min(which(log2FoldChange_rescaled_mean > 0.5))]
+    mid_induction = if (!any(log2FoldChange_rescaled_mean > 0.5)) max_induction else Time[min(which(log2FoldChange_rescaled_mean > 0.5))],
+    mean_variance = mean(log2FoldChange_rescaled_var_erki)
   ) %>%
   ungroup()
 
 plot_single_surface(
   lfc_long %>%
     inner_join(condition_meta, by = "condition") %>%
-    filter(gene_name == "FOSB")
+    filter(gene_name == "FOS")
 )
 
 plot_single_surface(
   temporal_ordering_avg_col_stats %>%
-    filter(gene_name == "FOSB") %>%
-    mutate(log2FoldChange_rescaled_var = log2FoldChange_rescaled*log2FoldChange_var),
-  aes(ERKi, Time, fill = log2FoldChange_rescaled_var)
+    filter(gene_name == "FOS") %>%
+    mutate(log2FoldChange_rescaled_norm = log2FoldChange_rescaled*log2FoldChange_var_time),
+  aes(ERKi, Time, fill = log2FoldChange_rescaled_norm)
 )
 
 hm_mid_vs_max <- temporal_ordering_avg_col %>%
