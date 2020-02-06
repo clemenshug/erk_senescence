0ae8926c9b61f4a7dcf7b2fc221da94804d6da3d
Clemens Hug <clemens.hug@gmail.com>
2020-02-06 10:59:41 -0500

    Add k-medoids clustering

--- analysis/cluster_kmedoids.R
+++ analysis/cluster_kmedoids.R
@@ -5,15 +5,16 @@ library(synExtra)
 library(polynom)
 library(broom)
 library(furrr)
+library(cluster)
 library(SimilarityMeasures)
 
 synLogin()
 syn <- synDownloader(here("tempdl"), followLink = TRUE)
 
-wd <- here("clustering", "linear_model_clustering")
+wd <- here("clustering", "kmedoids")
 dir.create(wd, recursive = TRUE, showWarnings = FALSE)
 
-syn_lm_clusters <- "syn21448567"
+syn_clustering <- "syn21432135"
 
 # set directories, import files ------------------------------------------------
 ###############################################################################T
@@ -49,6 +50,44 @@ pc_lfc_long <- lfc_long %>%
   inner_join(select(pca_values, condition, PC1, PC2), by = "condition")
   # inner_join(select(condition_merta, condition, PC1, PC2, erk_range), by = "condition")
 
+
+# Plotting funcs ---------------------------------------------------------------
+###############################################################################T
+
+plot_cluster_trajectories <- function(
+  data, x, y, condition_id, order_id, trajectory_id, facet_x = NULL, facet_y = NULL,
+  all_traces = FALSE
+) {
+  x_quo <- enquo(x)
+  y_quo <- enquo(y)
+  condition_id_quo <- enquo(condition_id)
+  trajectory_id_quo <- enquo(trajectory_id)
+  order_id_quo <- enquo(order_id)
+  facet_x_quo <- enquo(facet_x)
+  facet_y_quo <- enquo(facet_y)
+  averages <- data %>%
+    group_by(!!order_id_quo, !!condition_id_quo, !!facet_x_quo, !!facet_y_quo) %>%
+    summarize_at(vars(!!x_quo, !!y_quo), mean) %>%
+    ungroup() %>%
+    arrange(!!order_id_quo)
+  p <- ggplot(averages, aes(!!x_quo, !!y_quo, color = !!order_id_quo))
+  if (all_traces)
+    p <- p +
+    geom_path(
+      aes(group = !!trajectory_id_quo),
+      data = arrange(data, !!order_id_quo), size = .8, alpha = 0.1, color = "#CCCCCC"
+    )
+  p <- p +
+    geom_path() +
+    geom_point() +
+    theme_minimal() +
+    scale_color_viridis_c() +
+    theme(legend.position = "bottom")
+  if (!missing(facet_x) || !missing(facet_y))
+    p <- p +
+    facet_grid(vars(!!facet_y_quo), vars(!!facet_x_quo), scales = "free")
+  p
+}
 
 # Distance metrics -------------------------------------------------------------
 ###############################################################################T
@@ -130,28 +169,77 @@ distance_frechet <- function(df, groups, by, ...) {
 
 
 # Calculate distances ----------------------------------------------------------
-###############################################################################T\
+###############################################################################T
 
 pc_lfc_long_z <- pc_lfc_long %>%
   standardize_data(gene_id, log2FoldChange, PC1, PC2) %>%
   inner_join(condition_meta, by = "condition")
 
-
-
-pc_lfc_dist_frechet <- distance_frechet(
+pc_lfc_dist_euclidian <- distance_euclidian(
   pc_lfc_long_z %>%
     filter(Time == 24),
   gene_id, condition, PC1, PC2, log2FoldChange
 )
-write_rds(pc_lfc_dist_frechet, file.path(wd, "pc_lfc_frechet_dist.rds"))
+
+# pc_lfc_dist_frechet <- distance_frechet(
+#   pc_lfc_long_z %>%
+#     filter(Time == 24),
+#   gene_id, condition, PC1, PC2, log2FoldChange
+# )
+# write_rds(pc_lfc_dist_frechet, file.path(wd, "pc_lfc_frechet_dist.rds"))
 
 # pc_lfc_dist_frechet <- read_rds(file.path(wd, "pc_lfc_frechet_dist.rds"))
+
+# Do K-medoids -----------------------------------------------------------------
+###############################################################################T
+
+set.seed(42)
+cluster_euclidian_raw <- pam(pc_lfc_dist_euclidian, k = 8, diss = TRUE)
+
+cluster_euclidian_raw_df <- cluster_euclidian_raw %>%
+  chuck("clustering") %>%
+  enframe("gene_id", "cluster_id")
+
+plot_cluster_trajectories(
+  cluster_euclidian_raw_df %>%
+    inner_join(
+      pc_lfc_long_z %>%
+        filter(Time == 24),
+      by = "gene_id"
+    ) %>%
+    gather("PC", "PC_value", PC1, PC2) %>%
+    inner_join(select(pca_values, condition, PC1), by = "condition"),
+  PC_value, log2FoldChange, condition, PC1, gene_id, PC, cluster_id,
+  all_traces = TRUE
+)
+
+cluster_euclidian_identities <- tribble(
+  ~cluster_id, ~cluster, ~direction,
+  1, "high_erk", "-",
+  2, "low_erk", "-",
+  3, "full_range", "+",
+  4, "bell", "+",
+  5, "low_erk", "+",
+  6, "bell", "-",
+  7, "high_erk", "+",
+  8, "full_range", "-"
+) %>%
+  mutate(cluster_combined = paste(cluster, direction, sep = "_"))
+
+cluster_euclidian <- cluster_euclidian_raw_df %>%
+  inner_join(cluster_euclidian_identities, by = "cluster_id") %>%
+  select(-cluster_id)
+
+write_csv(
+  cluster_euclidian,
+  file.path(wd, "kmedoids_clustering_classes.csv")
+)
 
 # Store to synapse -------------------------------------------------------------
 ###############################################################################T
 
 
-lm_clustering_activity <- Activity(
+activity <- Activity(
   "Clustering ERK response genes based on their expression profile with increasing ERK concentration",
   used = c(
     "syn21432183",
@@ -159,11 +247,15 @@ lm_clustering_activity <- Activity(
     "syn21444456",
     "syn21444486"
   ),
-  executed = "https://github.com/clemenshug/erk_senescence/blob/master/analysis/lm_clusters.R"
+  executed = "https://github.com/clemenshug/erk_senescence/blob/master/analysis/cluster_kmedoids.R"
 )
 
+syn_kmedoids_clusters <- Folder("kmedoids_clusters", parent = syn_clustering) %>%
+  synStore() %>%
+  chuck("properties", "id")
+
 c(
-  file.path(wd, "lm_clustering_classes.csv")
+  file.path(wd, "kmedoids_clustering_classes.csv")
 ) %>%
-  synStoreMany(syn_lm_clusters, activity = lm_clustering_activity)
+  synStoreMany(syn_kmedoids_clusters, activity = activity)
 
